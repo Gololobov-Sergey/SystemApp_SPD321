20cb022fc160ad6d48b3546e045a49fe9f210c46
Сергій Гололобов <gololobov.serhiy@gmail.com>
2024-06-08 19:19:26 +0300

    Добавьте файлы проекта.

---
+++ SystemApp_SPD321/ExtFunc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemApp_SPD321_1
+{
+    public class ExtFunc
+    {
+        public const int MB_OKCANCEL = 1;
+        public const int MB_ICONQUESTION = 0x00000020;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindowByCaption(IntPtr hWnd, string lpWindowName);
+
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+}
---
+++ SystemApp_SPD321/Program.cs
@@ -0,0 +1,30 @@
+﻿using SystemApp_SPD321_1;
+
+namespace SystemApp_SPD321
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int res = ExtFunc.MessageBox(IntPtr.Zero, "Hello C#", "C# System", ExtFunc.MB_OKCANCEL | ExtFunc.MB_ICONQUESTION);
+            Console.WriteLine("You press: " + res.ToString());
+
+            string title = Console.ReadLine();
+            IntPtr hwnd = ExtFunc.FindWindowByCaption(IntPtr.Zero, title);
+            if (hwnd != IntPtr.Zero)
+            {
+                ExtFunc.ShowWindow(hwnd, 3);
+                Console.ReadKey();
+                ExtFunc.ShowWindow(hwnd, 6);
+                Console.ReadKey();
+                ExtFunc.ShowWindow(hwnd, 1);
+                Console.ReadKey();
+                ExtFunc.ShowWindow(hwnd, 5);
+                Console.ReadKey();
+
+            }
+            
+        }
+    }
+}
